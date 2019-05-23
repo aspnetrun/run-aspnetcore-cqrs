951000c1726f54298b5175f67c119e93c8df2254
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-05-23 14:48:01 +0300

    category list updated

--- src/AspnetRun.Web/src/app/views/category/category-list/category-list.component.ts
+++ src/AspnetRun.Web/src/app/views/category/category-list/category-list.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { CategoryDataService } from 'src/app/core/services/category-data.service';
 import { ActivatedRoute } from '@angular/router';
 import { ICategory } from 'src/app/shared/interfaces';
+import { ITabulatorTableOptions } from 'src/app/shared/tabulator-table/tabulator-table';
 
 @Component({
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent {
-  categories: ICategory[];
+  gridOptions: ITabulatorTableOptions = {
+    data: [],
+    columns: [
+      { title: "Id", field: "id" },
+      { title: "Name", field: "name" },
+      { title: "Description", field: "description" },
+    ]
+  }
 
   constructor(private dataService: CategoryDataService, route: ActivatedRoute) {
     route.params.subscribe(() => {
@@ -18,7 +26,7 @@ export class CategoryListComponent {
 
   getCategories() {
     this.dataService.getCategories().subscribe((categories: ICategory[]) => {
-      this.categories = categories;
+      this.gridOptions.data = categories;
     });
   }
 }
