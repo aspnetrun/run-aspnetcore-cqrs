9ece608ab1256c3ef50dc23134ee2d9fbb33fb39
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-07-23 14:06:57 +0300

    ng-wizard installed and implemented

--- src/AspnetRun.Web/src/app/app.module.ts
+++ src/AspnetRun.Web/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
+import { NgWizardModule, THEME } from 'ng-wizard';
+
 import { P404Component } from './views/error/404.component';
 import { P500Component } from './views/error/500.component';
 import { LoginComponent } from './views/login/login.component';
@@ -28,7 +30,8 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
     BrowserAnimationsModule,
     AppRoutingModule,     // Main routes for application
     CoreModule,           // Singleton objects (services, components that are loaded only once, etc.)
-    SharedModule,          // Shared (multi-instance) objects
+    SharedModule,         // Shared (multi-instance) objects
+    NgWizardModule.forRoot({ theme: THEME.default }),       // TODO: shall be moved  to core module
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/AspnetRun.Web/src/app/views/dashboard/dashboard.component.ts
+++ src/AspnetRun.Web/src/app/views/dashboard/dashboard.component.ts
@@ -1,9 +1,81 @@
 import { Component, OnInit } from '@angular/core';
+import { NgWizardStepDef, STEP_STATE, NgWizardConfig, THEME, StepChangedArgs, NgWizardService } from 'ng-wizard';
 
 @Component({
   templateUrl: 'dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
-  ngOnInit(): void {
+  steps: NgWizardStepDef[] = [
+    {
+      title: 'Step 1',
+      description: 'Step 1 description',
+      content: 'Step 1 Content',
+    },
+    {
+      title: 'Step 2',
+      description: 'Step 2 description',
+      content: 'Step 2 Content',
+      state: STEP_STATE.error,
+    },
+    {
+      title: 'Step 3',
+      description: 'Step 3 description',
+      content: 'Step 3 Content',
+      state: STEP_STATE.disabled,
+    },
+    {
+      title: 'Step 4',
+      description: 'Step 4 description',
+      content: 'Step 4 Content',
+      state: STEP_STATE.hidden,
+    },
+    {
+      title: 'Step 5',
+      description: 'Step 5 description',
+      content: 'Step 5 Content',
+    },
+  ];
+
+  config: NgWizardConfig = {
+    selected: 0,
+    theme: THEME.arrows,
+    toolbarSettings: {
+      toolbarExtraButtons: [
+        { text: 'Finish', class: 'btn btn-info', event: () => { alert("Finished!!!"); } },
+        { text: 'Reset', class: 'btn btn-danger', event: () => { this.resetWizard(); } }]
+    }
+  };
+
+  stepChangedArgs: StepChangedArgs;
+  selectedtheme: THEME;
+  themes = [THEME.default, THEME.arrows, THEME.circles, THEME.dots];
+
+  constructor(private ngWizardService: NgWizardService) {
+  }
+
+  ngOnInit() {
+    this.selectedtheme = this.config.theme;
+  }
+
+  showPreviousStep(event?: Event) {
+    this.ngWizardService.previous();
+  }
+
+  showNextStep(event?: Event) {
+    this.ngWizardService.next();
+  }
+
+  resetWizard(event?: Event) {
+    this.selectedtheme = this.config.theme;
+    this.ngWizardService.reset();
+  }
+
+  themeSelected() {
+    this.ngWizardService.theme(this.selectedtheme);
+  }
+
+  stepChanged(args: StepChangedArgs) {
+    console.log(args.step);
+    this.stepChangedArgs = args;
   }
 }
