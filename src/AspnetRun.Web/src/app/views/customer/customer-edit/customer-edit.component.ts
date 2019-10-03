fddd8b4bc4a5846e27f68b2db9a5177be2e1dbbf
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-10-03 10:41:38 +0300

    customer edit page created

--- src/AspnetRun.Web/src/app/views/customer/customer-edit/customer-edit.component.ts
+++ src/AspnetRun.Web/src/app/views/customer/customer-edit/customer-edit.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-customer-edit',
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { ICustomer, IState } from 'src/app/shared/interfaces';
+import { CustomerDataService } from 'src/app/core/services/customer-data.services';
+
+@Component({  
   templateUrl: './customer-edit.component.html',
   styleUrls: ['./customer-edit.component.css']
 })
 export class CustomerEditComponent implements OnInit {
 
-  constructor() { }
+  customer: ICustomer = 
+    {
+      id: 0,
+      firstName: '',
+      lastName: '',
+      gender: '',
+      address: '',
+      city: '',
+      state: {
+        abbreviation: '',
+        name: ''
+      }
+    };
+
+  states: IState[];
+  errorMessage: string;
+  deleteMessageEnabled: boolean;
+  operationText = 'Insert';
+  @ViewChild('customerForm', { static : true }) customerForm: NgForm;
+
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private dataService: CustomerDataService) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = +params['id'];
+      if (id !== 0) {
+        this.operationText = 'Update';
+        this.getCustomer(id);
+      }
+    });
+  }
+
+  getCustomer(id: number) {
+    this.dataService.getCustomerById(id).subscribe((customer : ICustomer) => { 
+      this.customer = customer 
+    });
   }
 
 }
