7413f27965e45be509dcbc910ed856069703dfd4
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-10-04 15:29:07 +0300

    SearchProductsRequestValidator added

---
+++ src/AspnetRun.Api/Application/Validations/SearchProductsRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using AspnetRun.Api.Requests;
+using FluentValidation;
+
+namespace AspnetRun.Api.Application.Validations
+{
+    public class SearchProductsRequestValidator : AbstractValidator<SearchProductsRequest>
+    {
+        public SearchProductsRequestValidator()
+        {
+            RuleFor(request => request.Args).NotNull();
+            RuleFor(request => request.Args.PageIndex).GreaterThan(0);
+            RuleFor(request => request.Args.PageSize).InclusiveBetween(10, 100);
+        }
+    }
+}
